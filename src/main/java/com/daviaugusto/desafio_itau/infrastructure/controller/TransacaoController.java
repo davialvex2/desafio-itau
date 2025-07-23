471ac20503fc141d7e0bbbae865cae42c26e7c18
Davi Augusto <davi.miranda0919@gmail.com>
2025-07-23 16:49:26 -0300

    end point de delete de transações e retorno de estatistica

--- src/main/java/com/daviaugusto/desafio_itau/infrastructure/controller/TransacaoController.java
+++ src/main/java/com/daviaugusto/desafio_itau/infrastructure/controller/TransacaoController.java
@@ -1,15 +1,13 @@
 package com.daviaugusto.desafio_itau.infrastructure.controller;
 
 
+import com.daviaugusto.desafio_itau.infrastructure.entities.Estatistica;
 import com.daviaugusto.desafio_itau.infrastructure.entities.Transacao;
 import com.daviaugusto.desafio_itau.infrastructure.services.TransacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transacao")
@@ -23,6 +21,17 @@ public class TransacaoController {
     public ResponseEntity<Void> gravarTrasacao(@RequestBody Transacao transacao){
         transacaoService.gravarTransacao(transacao);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarTrasacoes(){
+        transacaoService.deletarTransacoes();
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Estatistica> pegarEstatistica(){
+        return ResponseEntity.ok(transacaoService.pegarEstatisticas());
     }
 
 }
--- src/main/java/com/daviaugusto/desafio_itau/infrastructure/services/TransacaoService.java
+++ src/main/java/com/daviaugusto/desafio_itau/infrastructure/services/TransacaoService.java
@@ -1,11 +1,16 @@
 package com.daviaugusto.desafio_itau.infrastructure.services;
 
+import com.daviaugusto.desafio_itau.infrastructure.entities.Estatistica;
 import com.daviaugusto.desafio_itau.infrastructure.entities.Transacao;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 public class TransacaoService {
@@ -21,6 +26,32 @@ public class TransacaoService {
         }
             listaTransacao.add(transacao);
     }
+
+    public void deletarTransacoes(){
+        listaTransacao.clear();
+    }
+
+    public List<Transacao> ultimasTransacoes(List<Transacao> lista){
+        OffsetDateTime umMinutoTras = OffsetDateTime.now().minusMinutes(1);
+        List<Transacao> listaTransacao = lista.stream().filter(x -> x.getDataHora().isAfter(umMinutoTras)).collect(Collectors.toList());
+        return listaTransacao;
+    }
+
+    public Estatistica pegarEstatisticas(){
+
+        List<Transacao> listaEstatistica =  ultimasTransacoes(listaTransacao);
+        
+        List<Double> listaDouble = listaEstatistica.stream().map(x -> x.getValor()).toList();
+
+        DoubleSummaryStatistics stats = listaDouble.stream()
+                .mapToDouble(Double::doubleValue)
+                .summaryStatistics();
+
+        Estatistica estatistica = new Estatistica(stats.getCount(), stats.getSum(), stats.getAverage(), stats.getMin(), stats.getMax());
+
+        return estatistica;
+    }
+
 
 
 
